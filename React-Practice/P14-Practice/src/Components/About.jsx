c84767df6d4c50f43f342f81523b2ddce4b43930
shlok <shlok.sm@surya-soft.com>
2024-03-08 23:11:01 +0530

    Learnt Dynamic Routing

---
+++ React-Practice/P14-Practice/src/Components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="w-1/2 m-auto mt-10">
+      <h1 className=" text-red-200 text-5xl font-bold ">About</h1>
+      <p className="mb-2 text-zinc-500">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit placeat
+        inventore laudantium fugit, reiciendis repudiandae est cum, illum enim
+        ipsam excepturi eaque nisi. Veritatis rerum consectetur laudantium
+        suscipit voluptate consequatur.
+      </p>
+      <button className="text-white p-3 bg-zinc-300 rounded-md">
+        Explore More
+      </button>
+    </div>
+  );
+}
+
+export default About;
