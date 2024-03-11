c19ebfe00befbe1712a75f72c7388682aa6a3513
shlok <shlok.sm@surya-soft.com>
2024-03-11 23:26:19 +0530

    Learnt about useEffect()

--- React-Practice/P15-Practice/src/Components/Services.jsx
+++ React-Practice/P15-Practice/src/Components/Services.jsx
@@ -1,12 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../utils/Axios";
 
 function Services() {
+  const [products, setProducts] = useState([]);
+  const getProducts = () => {
+    axios
+      .get("/users")
+      .then((response) => {
+        console.log(response);
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   useEffect(() => {
-    console.log("Services started");
-
-    return () => console.log("Services stopped");
+    console.log("Show started");
+    getProducts();
+    return () => console.log("Show stopped");
   }, []);
-  return <div>Services</div>;
+  return (
+    <>
+      <ul className="mt-10 rounded-md">
+        {products.length > 0 ? (
+          products.map((item, index) => (
+            <li key={index} className="rounded-md mb-2 p-5 bg-red-200">
+              {item.username}
+            </li>
+          ))
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </ul>
+    </>
+  );
 }
 
 export default Services;
