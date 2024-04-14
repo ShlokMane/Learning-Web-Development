346642aa6fe292bf5ff86c49605f886f9fabcc6e
shlok <shlok.sm@surya-soft.com>
2024-04-14 23:34:53 +0530

    Learnt about useContext

---
+++ React-Practice/P16-Practice/src/context/Context.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+const Context = (props) => {
+  const [users, setUsers] = useState([
+    { id: 0, username: "Shlok", city: "Bengaluru" },
+    { id: 2, username: "Sanjay", city: "Mumbai" },
+    { id: 1, username: "Piyush", city: "Delhi" }, 
+  ]);
+
+  console.log(props);
+  return (
+    <UserContext.Provider value={{ users, setUsers }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export default Context;
